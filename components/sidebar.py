017f1c85ad835aa1e08d3c0d8f7625ca4f6f91a2
Disha <108977933+disha815@users.noreply.github.com>
2025-10-03 00:34:08 +0530

    new

--- components/sidebar.py
+++ components/sidebar.py
@@ -4,7 +4,7 @@ def show_sidebar_info():
     with st.sidebar:
         st.image("static/nhpc_logo.png", width=200)
         st.markdown("## 📊 Info")
-        st.write("Model: LLaMA 3.2")
+        st.write("Model: Gemma-3-27b-it ")
         st.write(f"Questions Asked: {len(st.session_state.chat_history)}")
         if "current_file" in st.session_state:
             st.write(f"PDF: {st.session_state.current_file}")
